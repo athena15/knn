f8b3b3e62f43af62135d73078aaf0313a942f9c0
athena15 <brenner.heintz@gmail.com>
2018-06-19 10:22:34 -0700

    Initial commit.

---
+++ wine.py
@@ -0,0 +1,26 @@
+import knn
+import numpy as np
+import pandas as pd
+import requests
+from io import StringIO
+
+# Import wine classification data from UC Irvine's website
+col_names = ['Alcohol', 'Malic acid', 'Ash', 'Alcalinity of ash', 'Magnesium', 'Total phenols', 'Flavanoids',
+             'Nonflavanoid phenols', 'Proanthocyanins', 'Color intensity', 'Hue', 'OD280/OD315 of diluted wines',
+             'Proline']
+url = 'https://archive.ics.uci.edu/ml/machine-learning-databases/wine/wine.data'
+s = requests.get(url).text
+df = pd.read_csv(StringIO(s), names=col_names)
+
+# Creating a column denoting whether a wine is above the median alcohol percentage
+def high_alc(x):
+	if x >= 13.05:
+		return 1
+	else:
+		return 0
+
+
+df['high_alcohol'] = df['Alcohol'].apply(high_alc)
+
+# Run k-nearest neighbors classification to predict whether a wine will be classified as 'high alcohol' or not.
+knn.nearest_neighbors_workflow(df, 'high_alcohol')
